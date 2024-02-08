e9ad16abbb2b921267531fa3c55074a544cfa4fd
S0025774733 <d.salavei@skybuffer.com>
2024-02-08 19:48:41 +0100

    working with hana db using credentials from xsa

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from cfenv import AppEnv
+from hdbcli import dbapi
 
 app = Flask(__name__)
 
@@ -8,16 +9,46 @@ app = Flask(__name__)
 port = int(os.environ.get('PORT', 52000))
 
 
-# GET request /hello
 @app.route('/', methods=['GET'])
 def hello():
+
     env = AppEnv()
     hdi_db_actionserver = env.get_service(name='hdi_db_actionserver')
     if hdi_db_actionserver is not None:
-        service = hdi_db_actionserver.credentials
+        cred = hdi_db_actionserver.credentials
+        db_address = cred.get('host')
+        db_port = cred.get('port')
+        db_user = cred.get('user')
+        db_password = cred.get('password')
+        db_scheme = cred.get('schema')
+
+        conn = dbapi.connect(
+            address=db_address,
+            port=db_port,
+            user=db_user,
+            password=db_password
+        )
+
+        db_error = ''
+
+        try:
+            sql = f'SELECT "OPERATION", "OPERATION_NAME" FROM "{db_scheme}"."Config.Operation"'
+            cursor = conn.cursor()
+            cursor.execute(sql)
+            cursor_data = cursor.fetchall()
+        except Exception as e:
+            db_error = f"DB ERROR: {e}"
+
+        cursor_string = ''
+
+        for row in cursor_data:
+            cursor_string += '\n' + str(row.column_values)
+
     else:
         service = ' hdi_db_actionserver is not found'
-    return f'name: {env.name};  port: {env.port}; space: {env.space}; service: {service}!'
+    response = make_response(f'env.name: {env.name};  env.port: {env.port}; env.space: {env.space}; service: {cred}; \n {cursor_string} \n {db_error}!')
+    response.headers['Content-Type'] = 'text/plain'
+    return response
 
 
 # GET request /greet with user parameter
