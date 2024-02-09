528366b84d3477d0f2fdb5d92c7cfadea1da2c28
S0025774733 <d.salavei@skybuffer.com>
2024-02-09 11:54:56 +0100

    making updates and getting results from hana

--- server.py
+++ server.py
@@ -11,7 +11,6 @@ port = int(os.environ.get('PORT', 52000))
 
 @app.route('/', methods=['GET'])
 def hello():
-
     env = AppEnv()
     hdi_db_actionserver = env.get_service(name='hdi_db_actionserver')
     if hdi_db_actionserver is not None:
@@ -29,10 +28,23 @@ def hello():
             password=db_password
         )
 
+        update_training_status(conn, db_scheme, 1)
+
         db_error = ''
+        cursor_data = []
 
         try:
-            sql = f'SELECT "OPERATION", "OPERATION_NAME" FROM "{db_scheme}"."Config.Operation"'
+            sql = f'''
+            SELECT "TENANTID",
+            "BOT_GUID",
+            "MODEL_GUID",
+            "MODEL_TYPE_ID",
+            "CREATEDAT",
+            "TRAINEDAT",
+            "STATUS_ID",
+            "LINK"
+            FROM "{db_scheme}"."Nlp.Model.Training"
+            '''  # "TRAINING_DATA"
             cursor = conn.cursor()
             cursor.execute(sql)
             cursor_data = cursor.fetchall()
@@ -46,7 +58,8 @@ def hello():
 
     else:
         service = ' hdi_db_actionserver is not found'
-    response = make_response(f'env.name: {env.name};  env.port: {env.port}; env.space: {env.space}; service: {cred}; \n {cursor_string} \n {db_error}!')
+    response = make_response(
+        f'env.name: {env.name};  env.port: {env.port}; env.space: {env.space}; service: {cred}; \n {cursor_string} \n {db_error}!')
     response.headers['Content-Type'] = 'text/plain'
     return response
 
@@ -64,6 +77,28 @@ def process():
     data = request.json  # Getting data from JSON request body
     # logic
     return jsonify({'message': 'Data processed successfully!'})
+
+
+def update_training_status(connection, scheme, new_status):
+    cursor = connection.cursor()
+
+    try:
+        # SQL request
+        sql_update_query = f"""
+        UPDATE "{scheme}"."Nlp.Model.Training"
+        SET "STATUS_ID" = '{new_status}',
+        "TRAINEDAT" = NOW() 
+        WHERE "MODEL_GUID" = '012F125996853000F0000003A7EE0300'
+        """
+
+        cursor.execute(sql_update_query)
+        connection.commit()
+
+    except Exception as e:
+        print(f"DB UPDATE ERROR: {e}")
+
+    finally:
+        cursor.close()
 
 
 if __name__ == '__main__':
